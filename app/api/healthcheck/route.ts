e750c5c16731fb9e02b0199b9dacc4775881561f
Gabor Posz <gabor.posz@gmail.com>
2025-07-16 21:23:00 +0200

    MongoDB connection was added

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -1,7 +1,22 @@
 "use server"
 
 import { NextResponse } from 'next/server';
+import { MongoClient } from 'mongodb';
 
 export async function GET() {
-  return NextResponse.json(true);
+  const dbPassword = "TTBfycDpQRCtlmla";
+  const uri = `mongodb+srv://WebPageUser:${dbPassword}@moonshinecluster-prod.bsp5mgb.mongodb.net/?retryWrites=true&w=majority&appName=MoonshineCluster-Prod`;
+  let client;
+  try {
+    client = new MongoClient(uri);
+    await client.connect();
+    await client.db().command({ ping: 1 });
+    return NextResponse.json(true);
+  } catch (error) {
+    return NextResponse.json(false);
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
 }
